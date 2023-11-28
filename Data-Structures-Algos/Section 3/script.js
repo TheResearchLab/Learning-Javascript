79009bdaf527cbc3a56208dcf17e9a79986380ef
TheResearchLab <aarontellis1996@outlook.com>
2023-11-28 06:54:47 -0500

    course: add notes for Big O notation and Scalibility section

---
+++ Data-Structures-Algos/Section 3/script.js
@@ -0,0 +1,19 @@
+const nemo = ['nemo'];
+const everyone = ['dory','bruce','marlin','nemo','gill','bloat','nigel','squirt','darla','hank']
+const large = new Array(10000).fill('marlin')
+large.push('nemo')
+
+
+function findNemo(array) {
+    let t0 = performance.now();
+    for (let i=0;i<array.length; i++) {
+        if (array[i] === 'nemo') {
+            console.log('found nemo!');
+        } 
+    }
+    let t1 = performance.now();
+    console.log(`The call to find Nemo took ${t1-t0} milliseconds`)
+}
+
+findNemo(nemo);
+findNemo(large);
