b03c14d8bfe718f67c3de0dabc0743a57daf9a15
TheResearchLab <aarontellis1996@outlook.com>
2023-11-29 19:24:42 -0500

    course: Big O notation and scalability continued. O(N) and O(1)

--- Data-Structures-Algos/Section 3/script.js
+++ Data-Structures-Algos/Section 3/script.js
@@ -1,19 +1,26 @@
-const nemo = ['nemo'];
-const everyone = ['dory','bruce','marlin','nemo','gill','bloat','nigel','squirt','darla','hank']
-const large = new Array(10000).fill('marlin')
-large.push('nemo')
+// const nemo = ['nemo'];
+// const everyone = ['dory','bruce','marlin','nemo','gill','bloat','nigel','squirt','darla','hank']
+// const large = new Array(10000).fill('marlin')
+// large.push('nemo')
 
+// // O(n) --> linear time
+// function findNemo(array) {
+//     for (let i=0;i<array.length; i++) {
+//         if (array[i] === 'nemo') {
+//             console.log('found nemo!');
+//         } 
+//     }
+// }
 
-function findNemo(array) {
-    let t0 = performance.now();
-    for (let i=0;i<array.length; i++) {
-        if (array[i] === 'nemo') {
-            console.log('found nemo!');
-        } 
-    }
-    let t1 = performance.now();
-    console.log(`The call to find Nemo took ${t1-t0} milliseconds`)
+// findNemo(nemo);
+// findNemo(large);
+
+// O(2) for two operations
+function logFirstTwoBoxes(boxes) {
+    console.log(boxes[0]); //O(1)
+    console.log(boxes[1]); //O(1)
 }
 
-findNemo(nemo);
-findNemo(large);
+const boxes = new Array(100).fill('things');
+
+logFirstTwoBoxes(boxes);
