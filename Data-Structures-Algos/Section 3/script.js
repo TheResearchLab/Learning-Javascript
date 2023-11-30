35bd8be19311952977e31717493580cef26bd0c4
TheResearchLab <aarontellis1996@outlook.com>
2023-11-30 07:09:57 -0500

    course:Continue Big O rules and examples.

--- Data-Structures-Algos/Section 3/script.js
+++ Data-Structures-Algos/Section 3/script.js
@@ -6,8 +6,10 @@
 // // O(n) --> linear time
 // function findNemo(array) {
 //     for (let i=0;i<array.length; i++) {
+//         console.log('running')
 //         if (array[i] === 'nemo') {
 //             console.log('found nemo!');
+//             break;
 //         } 
 //     }
 // }
@@ -15,12 +17,54 @@
 // findNemo(nemo);
 // findNemo(large);
 
-// O(2) for two operations
-function logFirstTwoBoxes(boxes) {
-    console.log(boxes[0]); //O(1)
-    console.log(boxes[1]); //O(1)
+// // O(2) for two operations
+// function logFirstTwoBoxes(boxes) {
+//     console.log(boxes[0]); //O(1)
+//     console.log(boxes[1]); //O(1)
+// }
+
+// const boxes = new Array(100).fill('things');
+
+// logFirstTwoBoxes(boxes);
+
+
+// Big O Rule #3 - watch for different terms for inputs
+// function compressBoxes(boxes,boxes2) {
+//     boxes.forEach(function(boxes) {
+//         console.log(boxes);
+//     })
+
+//     boxes2.forEach(function(boxes) {
+//         console.log(boxes);
+//     })
+// }
+// // Big O of O(a+b)
+
+
+// boxArr1 = new Array(100).fill('stuff');
+// boxArr2 = new Array(1000).fill('things');
+
+// compressBoxes(boxArr1,boxArr2);
+
+
+// Loop Nested inside of a loop O(n^2)
+const boxes = [1,2,3,4,5];
+
+// const logPairs = function(boxes) {
+//     for (let i=0; i< boxes.length; i++ ) {
+//         for (let j=0; j<boxes.length; j++) {
+//             console.log(`${boxes[i]}, ${boxes[j]}`)
+//         }
+//     }
+// }
+const logPairs = function(boxes) {
+    boxes.forEach(function(firstBox) {
+        boxes.forEach(function(secondBox) {
+            console.log(firstBox,secondBox);
+        })
+    })
 }
 
-const boxes = new Array(100).fill('things');
 
-logFirstTwoBoxes(boxes);
+
+logPairs(boxes)
