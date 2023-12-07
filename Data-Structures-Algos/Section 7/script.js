92f6d5d3eb8670466b6273a1c6d57051955dda46
TheResearchLab <aarontellis1996@outlook.com>
2023-12-07 08:11:43 -0500

    course: start section 7 on hashing

---
+++ Data-Structures-Algos/Section 7/script.js
@@ -0,0 +1,13 @@
+ let user = {
+    age: 54,
+    name: 'Kylie',
+    magic: true,
+    scream: function() {
+        console.log('ahhhhhh');
+    }
+ }
+
+ user.age;
+ user.spell = 'abra kadabra';
+console.log(user);
+user.scream(); 
