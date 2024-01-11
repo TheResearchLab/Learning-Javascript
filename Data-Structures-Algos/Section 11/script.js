6ec86d2d0f9a12986a48d87958bd6450f39ddb73
TheResearchLab <aarontellis1996@outlook.com>
2024-01-10 23:38:14 -0500

    course: VIDEO-149 Create example graph data representations

---
+++ Data-Structures-Algos/Section 11/script.js
@@ -0,0 +1,13 @@
+ //edge list
+ const edgeGraph = [[0,2],[2,3],[2,1],[1,3]];
+
+ //Adjacent list 
+ const adjGraph = [[2],[2,3],[3,4],[12,5]] // node values are the index values themselves
+
+// Adjacent Matrix 
+const graph = [
+    [0,1,1,0],
+    [1,1,0,0],
+    [0,0,0,1],
+    [1,0,0,0]
+]
