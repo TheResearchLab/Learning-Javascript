ab0c327365222063bd636fd4f96a8420959c424a
TheResearchLab <aarontellis1996@outlook.com>
2024-01-13 15:17:11 -0500

    course: VIDEO-154 finish graphs and section on data structures

--- Data-Structures-Algos/Section 11/script.js
+++ Data-Structures-Algos/Section 11/script.js
@@ -11,3 +11,37 @@ const graph = [
     [0,0,0,1],
     [1,0,0,0]
 ]
+
+// Implementing Graph with Adjacency list
+
+
+
+class Graph {
+    constructor() {
+        this.numberOfNodes = 0;
+        this.adjacentList = {};
+    }
+    addVertex(node) {
+        this.adjacentList[node] = new Array(0);
+        this.numberOfNodes++;
+        return this;
+    }
+    addEdge(node1,node2) {
+        if(!this.adjacentList[node1] || !this.adjacentList[node2]) {
+            return `you entered a non-valid node`
+        } 
+        
+        this.adjacentList[node1].push(node2);
+        this.adjacentList[node2].push(node1);
+        return this
+
+    }
+    showCollections() {   
+    }
+}
+
+const myGraph = new Graph();
+console.log(myGraph.addVertex(6));
+console.log(myGraph.addVertex(12));
+console.log(myGraph.addEdge(4,12));
+
