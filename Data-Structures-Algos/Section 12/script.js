3a7d59f28edbb830ae0a3582110b33f6df501441
TheResearchLab <aarontellis1996@outlook.com>
2024-01-17 08:02:58 -0500

    feat: VIDEO-161 Fibinnacci Exercise

--- Data-Structures-Algos/Section 12/script.js
+++ Data-Structures-Algos/Section 12/script.js
@@ -1,7 +1,37 @@
+let counter = 0;
 function inception() {
-    debugger;
-    inception();
+    console.log(counter);
+    if(counter>3){
+        return 'done!'
+    }
+    counter++;
+    return inception();
 }
 
 
-inception();
+console.log(inception());
+
+
+function findFactorialRecursive(number) {
+    if(number === 2) {
+        return 2;
+    }
+    return number * findFactorialRecursive(number-1)
+}
+
+function findFactorialIterative(number) {
+    let answer = 1;
+    for(let i=2;i<=number;i++) {
+        answer = answer * i
+    }
+    
+    return answer;
+}
+
+console.log(findFactorialIterative(5)); // 120
+console.log(findFactorialRecursive(5)); // 
+
+
+function fibonacciRecursion(n) {
+    
+}
