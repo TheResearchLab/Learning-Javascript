2955f87b059bfc5920e9cbe9f912ab9489aa4f54
TheResearchLab <aarontellis1996@outlook.com>
2024-01-21 21:57:09 -0500

    course: VIDEO-166 Recursion Review

--- Data-Structures-Algos/Section 12/script.js
+++ Data-Structures-Algos/Section 12/script.js
@@ -9,7 +9,7 @@ function inception() {
 }
 
 
-console.log(inception());
+// console.log(inception());
 
 
 function findFactorialRecursive(number) {
@@ -28,10 +28,24 @@ function findFactorialIterative(number) {
     return answer;
 }
 
-console.log(findFactorialIterative(5)); // 120
-console.log(findFactorialRecursive(5)); // 
+// console.log(findFactorialIterative(5)); // 120
+// console.log(findFactorialRecursive(5)); // 
 
+
+function fibonacciIterative(n) {
+    let arr = [0,1];
+    for(let i=2;i<n+1;i++) {
+        arr.push(arr[i-2] + arr[i-1])
+    }
+    return arr[n]
+}
 
 function fibonacciRecursion(n) {
-    
+    if(n<2) {
+        return n
+    }
+    return fibonacciRecursion(n-1) + fibonacciRecursion(n-2); 
 }
+
+// console.log(fibonacciRecursion(8));
+console.log(fibonacciIterative(8));
