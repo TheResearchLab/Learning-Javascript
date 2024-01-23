8f50fd2dbf3c6f9c4f68a5e6ed718dbe5d896387
TheResearchLab <aarontellis1996@outlook.com>
2024-01-23 07:13:36 -0500

    course: VIDEO-182 Stable vs Unstable Algorithms

--- Data-Structures-Algos/Section 13/script.js
+++ Data-Structures-Algos/Section 13/script.js
@@ -52,13 +52,53 @@ function selectionSort(arr) {
 console.log(selectionSort(numbers));
 
 function insertionSort(arr) {
-    const sortedArr = [];
     const length = arr.length;
-    for(let i=0;i<length;i++) {
-        if(arr[i]>arr[i+1]) {
-            sortedArr[]
-            arr[i] = arr[i+1];
-            arr[i+1] = temp
+    for (let i=0;i<length;i++) {
+        if (arr[i] < arr[0]) {
+            arr.unshift(arr.splice(i,1)[0]);
+        } else {
+            for (let j=1;j<i;j++) {
+                if (arr[i] > arr[j-1] && arr[i] < arr[j]) {
+                    arr.splice(j,0,arr.splice(i,1)[0]);
+                }
+            }
         }
-    }                                                                            
+    }
+    return arr                                                      
+}
+
+console.log(insertionSort([1,3,2]));
+
+// function merge sort
+
+function mergeSort(arr) {
+    if(arr.length ===1) {
+        return arr
+    }
+
+    const length = arr.length;
+    const middle = Math.floor(length/2);
+    const left = arr.slice(0,middle);
+    const right = arr.slice(middle);
+
+    return mergeSort(
+        mergeSort(left),
+        mergeSort(right)
+    )
+}
+
+function merge(left,right) {
+    const result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    while(leftIndex<left.length && rightIndex < right.length) {
+        if(left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(right[rightIndex]);
+            rightIndex++
+        }
+    }
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex))
 }
