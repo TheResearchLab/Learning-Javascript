6c887f21d263d595db9c1395ace385ba15060364
TheResearchLab <aarontellis1996@outlook.com>
2024-02-04 14:33:13 -0500

    course: VIDEO-221 finish dynamic programming section

--- Data-Structures-Algos/Section 15/script.js
+++ Data-Structures-Algos/Section 15/script.js
@@ -21,3 +21,30 @@ const memoized = memoizedAddTo80();
 
 console.log('1',memoized(5));
 console.log('2',memoized(5));
+
+function fibonacci(n) {
+  if(n<2) {
+    return n
+  }
+  return fibonacci(n-1) + fibonacci(n-2)
+}
+
+function fibonacciMaster() {
+  let cache = {};
+  return function fib(n) {
+    if(n in cache) {
+      return cache[n]
+    } else {
+      if(n<2) {
+        return n
+      } else {
+        cache[n] = fib(n-1) + fib(n-2);
+        return cache[n]
+      }
+
+    }
+  }
+}
+
+const fasterFib = fibonacciMaster();
+console.log('DP',fasterFib(10));
