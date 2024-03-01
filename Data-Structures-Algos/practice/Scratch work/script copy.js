7edf32d0b3f0389116a3a9686ce394e3113b06f3
TheResearchLab <aarontellis1996@outlook.com>
2024-02-29 20:06:00 -0500

    practice: data structures and algorithms practice notes

---
+++ Data-Structures-Algos/practice/Scratch work/script copy.js
@@ -0,0 +1,26 @@
+// 0 1 1 2 3 5 8
+
+// Recursion Algo
+function fibonacci(n) {
+    if (n <= 1) {
+        return n;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+// Bubble Sort
+function bubbleSort(arr) {
+    const len = arr.length;
+    for (let i = 0; i < len - 1; i++) {
+        for (let j = 0; j < len - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
