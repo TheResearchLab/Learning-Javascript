3a102f708027fa1591a53e56a971f8a11fc8921d
Aaron Tellis <aarontellis1996@outlook.com>
2024-09-24 07:48:18 -0400

    reading: CH-2 unicode, reserve words, and semicolons (pg. 21)

---
+++ Books/Definitive-JS-Guide/LexicalStructure.js
@@ -0,0 +1,15 @@
+// unicode escape sequence
+
+let café = 1;
+
+console.log(café,caf\u00e9,caf\u{E9})
+console.log("\u{1F600}")
+
+// implicit semicolons 
+let a 
+a 
+= 
+3
+console.log(a)
+// treated as let a; a = 3; console.log(a);
+
