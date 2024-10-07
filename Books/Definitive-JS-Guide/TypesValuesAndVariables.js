31810b8f235112ea4a1149f56ff5d184447561db
Aaron Tellis <aarontellis1996@outlook.com>
2024-10-07 07:12:44 -0400

    reading: CH-3 string methods continued, regex, and boolean values (pg.40)

--- Books/Definitive-JS-Guide/TypesValuesAndVariables.js
+++ Books/Definitive-JS-Guide/TypesValuesAndVariables.js
@@ -57,6 +57,7 @@ console.log(singleLine)
 
 
 let s = "Hello World"
+let e = "😊"
 s.length 
 
 // taking portions of a string
@@ -75,4 +76,68 @@ s.startsWith("Hello")
 s.endsWith("!")
 s.includes("you") 
 
-//
+// modifying a string
+s.replace("llo","ya")
+s.toLowerCase()
+s.toUpperCase()
+console.log(s.normalize()) // unicode NFC normalization?
+console.log(s.normalize('NFD')) // normalize with different standard?
+
+// Inspecting Individual (16-bit) characters  of string?
+console.log(s.charAt(s.length-1)) // d
+s.charAt(0)
+console.log(s.charCodeAt(0)) // 16-bit number representing the character at index X
+console.log(e.codePointAt(0)) // ES6 works for codepoints > 16-bits like
+
+// Add space to beginning and end of string
+console.log("x".padStart(3,'😊')) // add space from the start upto character such that the total length is 3 and fill space with smiley
+console.log("x".padEnd(7,"ox")) // add and characters to the end
+
+// trim
+console.log("  test  ".trim())
+console.log("  test  ".trimStart())
+console.log("  test  ".trimEnd())
+
+// other misc string methods and features
+console.log(s.concat("!")) // can do this in multiple ways
+console.log("<>".repeat (5)) // repeat the give string 5 times
+console.log("string"[0]);
+
+// Tagged Template Literals?
+`\n`.length;
+String.raw`\n`.length;
+
+// Pattern Matching
+let text = "testing: 1, 2, 3";
+let pattern = /\d+/g; // match all instances of one or more digits?
+console.log(pattern.test(text)); // true a match exists
+console.log(text.search(pattern)); // position of first match
+console.log(text.match(pattern)); // return array of all matches
+console.log(text.replace(pattern,"#")); //replace matches with fill character(s)
+console.log(text.split(/\D+/)) // split on nondigits? Not Understanding why space in first index.
+
+// Boolean Values
+// Below values resolve to false 
+undefined 
+0
+null
+NaN
+""
+-0 
+
+if("value" !== null){ // has to be a specific falsy ie. null
+    console.log("hello I ran");
+}
+
+// is more strict than
+
+if (!"value") { // evals for any falsy
+    console.log("hello");
+}
+
+
+
+
+
+
+
